64114c378724197b47dfd2f135f1b5fc2e6242f2
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-08-21 23:52:05 +0700

    Update BoosteroidDesktop.bat

--- BoosteroidDesktop.bat
+++ BoosteroidDesktop.bat
@@ -1,5 +1,6 @@
 net user Admin Thuonghai001
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
-curl -O https://raw.githubusercontent.com/TheDarkMythos/Ngrok-Exe/master/ngrok.exe
-ngrok authtoken UqkqD5EBxWPggw7xGAiQ_77M4D5kjaPNdr9wdb3m9z
-ngrok tcp --region ap 3389
+@curl -O https://raw.githubusercontent.com/TheDarkMythos/Ngrok-Exe/master/ngrok.exe
+@ngrok authtoken UqkqD5EBxWPggw7xGAiQ_77M4D5kjaPNdr9wdb3m9z
+@ngrok tcp --region ap 3389
+pause
