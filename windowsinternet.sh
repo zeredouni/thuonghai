18a447454497ed255cadce2c93c8b900646c8a80
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-08-26 23:08:46 +0700

    Update windowsinternet.sh

--- windowsinternet.sh
+++ windowsinternet.sh
@@ -6,3 +6,4 @@ nohup ngrok tcp --region ap 10.10.20.90:3389 &>/dev/null &
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo Administrator
 echo C1sco12345
+echo "10.10.20.50/developer/C1sco12345"
