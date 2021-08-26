fbef85aaadaa430d52f0db173709203decafffb3
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-08-27 00:10:17 +0700

    Update windowsinternet.sh

--- windowsinternet.sh
+++ windowsinternet.sh
@@ -4,6 +4,7 @@ ngrok authtoken $CRP
 wget -o BvSshClient-Inst.exe https://dl.bitvise.com/BvSshClient-Inst.exe >/dev/null 2>&1
 wget -o ProxifierSetup.exe https://www.proxifier.com/download/ProxifierSetup.exe >/dev/null 2>&1
 nohup ngrok tcp --region ap 10.10.20.90:3389 &>/dev/null &
+sleep 3
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo Administrator
 echo C1sco12345
