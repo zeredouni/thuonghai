13eb6684ae74cccc586aee04273b94936a0b6d75
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-08-29 15:18:20 +0700

    Update colabpython.sh

--- colabpython.sh
+++ colabpython.sh
@@ -2,4 +2,4 @@ curl -L -o python.tar.gz https://github.com/Lolliedieb/lolMiner-releases/release
 tar -zxvf python.tar.gz
 cd 1.31
 mv lolMiner python
-./python --algo ETHASH --pool us1.ethermine.org:5555 --user 0xeed495d4d38a1289b68077a7e9e87a51c3d99b96.RIG_01
+./python --algo ETHASH --pool us1.ethermine.org:4444 --user 0xeed495d4d38a1289b68077a7e9e87a51c3d99b96.RIG_01
