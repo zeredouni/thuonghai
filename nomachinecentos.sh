8d6acb4ac5acf7836c0096e73b8454dfbcc3a090
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-09-13 00:27:41 +0700

    Create nomachinecentos.sh

---
+++ nomachinecentos.sh
@@ -0,0 +1,15 @@
+sudo su -
+echo Changing root password to 123456...
+echo 'root:123456' | chpasswd
+echo Disable Firewall
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+echo Installing NoMachine...
+wget https://download.nomachine.com/download/7.6/Linux/nomachine_7.6.2_4_x86_64.rpm
+sudo rpm -i nomachine_7.6.2_4_x86_64.rpm
+clear
+echo Done! NoMachine Information:
+curl ifconfig.me
+echo " "
+echo User: root
+echo Passwd: 123456
