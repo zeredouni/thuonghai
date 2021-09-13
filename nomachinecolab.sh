41c6cba11d8214edac4a941e6f19117b9f50de6d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-09-13 13:53:51 +0700

    Create nomachinecolab.sh

---
+++ nomachinecolab.sh
@@ -0,0 +1,27 @@
+wget -O ngrok-stable-linux-amd64.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip
+clear
+read -p "Paste authtoken here (Copy and Right-click to paste): " CRP
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp --region ap 4000 &>/dev/null &
+apt install --assume-yes xfce4 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+echo Changing root password to 123456...
+echo 'root:123456' | chpasswd
+echo 'student:123456' | chpasswd
+echo Adding student into root user...
+usermod -aG wheel student
+echo Disabling Firewall...
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+echo Installing NoMachine...
+wget https://download.nomachine.com/download/7.6/Linux/nomachine_7.6.2_4_amd64.deb
+sudo dpkg -i nomachine_7.6.2_4_amd64.deb
+clear
+echo Done! NoMachine Information:
+echo IP Address:
+curl ifconfig.me
+echo " "
+echo User: root
+echo Passwd: 123456
