0ce71fda19f427f553701abc2c1b1cd94fa352ea
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-09-15 16:22:56 +0700

    Create DOGE_unminerable.sh

---
+++ DOGE_unminerable.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-x64.tar.gz
+tar xf xmrig-6.15.0-linux-x64.tar.gz
+cd xmrig-6.15.0
+sudo ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DDspGoSAP4MAs8A195kofkTauxJvhPqemi.WORKER01#dsfz-a7ys -p x
