eec63cef7c3d1bf24b224d5a72e44210245317a9
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-10-04 00:48:58 +0700

    Update katacodaNG.sh

--- katacodaNG.sh
+++ katacodaNG.sh
@@ -2,7 +2,7 @@ clear
 echo "======================="
 echo "Download ngrok"
 echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget --no-check-certificate -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 read -p "Paste Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
