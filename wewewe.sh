35ef97ed3f735684aedb782037c2aa649abb0294
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-10-06 14:09:36 +0700

    Update wewewe.sh

--- wewewe.sh
+++ wewewe.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -p 6080:80 --device /dev/snd -e ALSADEV=hw:2,0 dorowu/ubuntu-desktop-lxde-vnc > /dev/null 2>&1
+sudo docker run -it --rm -p 6080:80 --device /dev/snd -e ALSADEV=hw:2,0 dorowu/ubuntu-desktop-lxde-vnc > /dev/null 2>&1
