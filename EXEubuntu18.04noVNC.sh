4814e84e21b399d43b9f9ea6979752b84b85840f
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-10-07 20:35:35 +0700

    Update EXEubuntu18.04noVNC.sh

--- EXEubuntu18.04noVNC.sh
+++ EXEubuntu18.04noVNC.sh
@@ -6,4 +6,4 @@ sudo docker exec -i ubuntu18.04 /bin/bash -c "apt update && apt remove -y chromi
 echo "Installing CrossOver (Run Windows Application)..."
 sudo docker exec -i ubuntu18.04 /bin/bash -c "wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_21.0.0-1.deb && dpkg -i crossover_21.0.0-1.deb ; apt install --assume-yes --fix-broken" > /dev/null 2>&1
 clear 
-echo All done! Please click View At Port 8080 to access your VM!
+echo "All done! Please click Web Preview > Preview On Port 8080 to access your VM!"
