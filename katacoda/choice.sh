4816a23c5b6ca1f86e2e39e03a7804e541611875
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-10-14 20:02:58 +0700

    Update choice.sh

--- katacoda/choice.sh
+++ katacoda/choice.sh
@@ -32,7 +32,7 @@ echo "IP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' > tunnel.txt
 cat tunnel.txt | sed 's/^.*:\([0-9]*\)$/\1/' > port.txt
 rg=$(echo cat RG.txt | bash)
-curl -s -H 'accept: application/dns-json' 'https://dns.google/resolve?name=0.tcp.$rg.ngrok.io&type=A' | jq -r '.Answer[0].data' > ip.txt
+dig +short 0.tcp.$rg.ngrok.io > ip.txt
 ip=$(echo cat ip.txt | bash)
 port=$(echo cat port.txt | bash)
 echo $ip:$port
