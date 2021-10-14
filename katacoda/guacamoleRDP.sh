f30844fa836d55f50a48a08cfe40c64ddb386b73
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-10-14 01:41:49 +0000

    Update guacamoleRDP.sh

--- katacoda/guacamoleRDP.sh
+++ katacoda/guacamoleRDP.sh
@@ -1,8 +1,8 @@
     apt install zip -y 
-    curl -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/guacamole.zip
-    curl -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/guacamole.z01
-    zip -F guacamole.zip --out file-large.zip
-    unzip file-large.zip
+    curl -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/guacamole.zip > /dev/null 2>&1
+    curl -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/guacamole.z01 > /dev/null 2>&1
+    zip -F guacamole.zip --out file-large.zip > /dev/null 2>&1
+    unzip file-large.zip > /dev/null 2>&1
     sudo docker login --username=tylermaymr18 --password=Thuonghai001 > /dev/null 2>&1
     docker run --network host -d -v /root/guacamole:/config oznu/guacamole > /dev/null 2>&1
     
