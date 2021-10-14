20746f256e41d0476f4b02dcec38eb033b1600bd
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-10-14 17:37:49 +0700

    Update choice.sh

--- katacoda/choice.sh
+++ katacoda/choice.sh
@@ -26,7 +26,8 @@ sleep 2
 rm -rf ngrok-stable-linux-amd64.zip
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels > /dev/null 2>&1 || bash -c "echo An Error With Your Tunnel, Pleasy Try Again... && sleep 2 && bash choice.sh"
 clear
-echo All done! Please click Open Desktop to access your VM!
+echo All done! Here your VM!
+IP:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 curl -L -s -o spinner.sh https://bit.ly/3AEOj7j > /dev/null 2>&1
 chmod +x spinner.sh
