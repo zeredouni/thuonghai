c91302dd62d01f85366113852b3bdb087cfa26fb
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-10-14 16:46:49 +0700

    Update choice.sh

--- katacoda/choice.sh
+++ katacoda/choice.sh
@@ -1,3 +1,5 @@
+trap '' 2 
+
 echo Please Wait...
 wget -O ngrok-stable-linux-amd64.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1 
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
@@ -18,4 +20,9 @@ echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose your region: " CRP
 ./python3 tcp --region $CRP 3389 &>/dev/null &
+clear
+echo All done! Please click Open Desktop to access your VM!
+curl -L -s -o spinner.sh https://bit.ly/3AEOj7j > /dev/null 2>&1
+chmod +x spinner.sh
+./spinner.sh sleep 999999
 
