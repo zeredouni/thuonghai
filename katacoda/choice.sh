fe5fcb3c31f1d256c1846420616b1f4ca9ed833d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-10-19 20:10:40 +0700

    Update choice.sh

--- katacoda/choice.sh
+++ katacoda/choice.sh
@@ -1,5 +1,5 @@
-[ -s ok ] || echo "Just a moment..." 
-echo done > ok
+#[ -s ok ] || echo "Just a moment..." 
+#echo done > ok
 curl -s -L -o choiceregion.sh https://bit.ly/3aB2jUX
 chmod +x choiceregion.sh
 ./choiceregion.sh
