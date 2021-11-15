210db6625533b4fa776604c50782dc1d7edecce4
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-15 08:13:45 +0700

    Update noVNC.sh

--- katacoda/course6/noVNC.sh
+++ katacoda/course6/noVNC.sh
@@ -6,5 +6,5 @@ wget https://github.com/kmille36/thuonghai/raw/master/katacoda/index.html > /dev
 mv index.html noVNC/index.html
 echo Starting noVNC Server...
 nohup ./noVNC/utils/novnc_proxy --listen 80 --vnc localhost:5901 &>/dev/null &
-echo All done! Please click + > View HTTP port 80 on Host 1 to access your VM!
+echo "All done! Please click + > View HTTP port 80 on Host 1 to access your VM!"
 
