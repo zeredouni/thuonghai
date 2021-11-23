9bfef43ba17040b33838c5bf30eff7ca56400c4a
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-24 00:20:32 +0700

    Update psiphon.bat

--- psiphon.bat
+++ psiphon.bat
@@ -1,5 +1,7 @@
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 curl -L -k -O https://github.com/kmille36/thuonghai/raw/master/setproxywin.bat
+cd "C:\Users\Public\Desktop"
+curl -L -k -o "EnableInternet.bat" https://github.com/kmille36/thuonghai/raw/master/setproxywin.bat
 cd "C:\PerfLogs"
 echo cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup" > start.bat
 echo curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip  >> start.bat
