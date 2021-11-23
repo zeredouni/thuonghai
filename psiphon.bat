bbc3cf5764b4c8a91172e98e699df90caba1ceda
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-23 22:16:19 +0700

    Update psiphon.bat

--- psiphon.bat
+++ psiphon.bat
@@ -1,5 +1,6 @@
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
-echo curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip  > start.bat
+echo cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup" > start.bat
+echo curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip  >> start.bat
 echo tar xf Psiphon3.zip >> start.bat
 echo cd Psiphon3 >> start.bat
 echo start vpn.bat >> start.bat
