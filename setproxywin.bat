b0abb65069ec9d0d59bb107a90535c6c3b071efd
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-23 23:58:44 +0700

    Update setproxywin.bat

--- setproxywin.bat
+++ setproxywin.bat
@@ -1,3 +1,4 @@
+@echo off
 REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "localhost:8080" /t REG_SZ /f
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /d "1" /t REG_SZ /f
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxySettingsPerUser /d "0" /t REG_SZ /f
