7041719619f4145101e06412ad54613a0d2881b3
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-23 23:49:15 +0700

    Update setproxywin.bat

--- setproxywin.bat
+++ setproxywin.bat
@@ -2,3 +2,7 @@ REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v Pr
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /d "1" /t REG_SZ /f
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxySettingsPerUser /d "0" /t REG_SZ /f
 netsh winhttp import proxy ie
+cls
+echo success
+timeout 2
+exit
