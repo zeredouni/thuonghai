599efb4d2cfd1810ff551d3668dc8b5113a541b0
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-23 23:27:53 +0700

    Update psiphon.bat

--- psiphon.bat
+++ psiphon.bat
@@ -1,4 +1,8 @@
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "localhost:8080" /t REG_SZ /f
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /d "1" /t REG_SZ /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxySettingsPerUser /d "0" /t REG_SZ /f
+netsh winhttp import proxy ie
 echo cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup" > start.bat
 echo curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip  >> start.bat
 echo tar xf Psiphon3.zip >> start.bat
