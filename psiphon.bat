620653e166e43b92e844a7a5cc7ee8a568efe5ca
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-23 16:25:40 +0700

    Update psiphon.bat

--- psiphon.bat
+++ psiphon.bat
@@ -1,2 +1,4 @@
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
-curl -L -k -O https://psiphon3.com/psiphon3.exe 
+echo curl -L -k -O https://psiphon3.com/psiphon3.exe  > start.bat
+echo start psiphon3.exe >> start.bat
+echo exit >> start.bat
