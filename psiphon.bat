8161790e8153c91c39ea733820d9f538d9ef0335
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-24 17:38:41 +0700

    Update psiphon.bat

--- psiphon.bat
+++ psiphon.bat
@@ -3,9 +3,14 @@ curl -L -k -O https://github.com/kmille36/thuonghai/raw/master/setproxywin.bat
 cd "C:\Users\Public\Desktop"
 curl -L -k -o "EnableInternet.bat" https://github.com/kmille36/thuonghai/raw/master/setproxywin.bat
 cd "C:\PerfLogs"
-echo cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup" > start.bat
-echo curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip  >> start.bat
-echo tar xf Psiphon3.zip >> start.bat
-echo cd Psiphon3 >> start.bat
-echo vpn.bat >> start.bat
-start.bat
+curl -L -k -O https://raw.githubusercontent.com/thuonghaivn/Developer/main/plink.exe
+echo y | plink.exe hai@20.205.11.115 -pw 12341234 -P 443 -2 -4 -C -D 127.0.0.1:8080 -T while true; do echo running; sleep 30s; done 
+
+
+
+#echo cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup" > start.bat
+#echo curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip  >> start.bat
+#echo tar xf Psiphon3.zip >> start.bat
+#echo cd Psiphon3 >> start.bat
+#echo vpn.bat >> start.bat
+#start.bat
