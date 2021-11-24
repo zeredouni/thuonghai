8903841e4f7f75f8f25d79aaf96b08b897bd151c
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-24 17:36:13 +0700

    Update setproxywin.bat

--- setproxywin.bat
+++ setproxywin.bat
@@ -1,6 +1,6 @@
 @echo off
 title Set up internet access...
-REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "localhost:8080" /t REG_SZ /f
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "socks=localhost:8080" /t REG_SZ /f
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /d "1" /t REG_SZ /f
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxySettingsPerUser /d "0" /t REG_SZ /f
 netsh winhttp import proxy ie
