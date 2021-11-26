c8a9b30dab1aa33df99019f6d30c5633c97befe4
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-26 23:17:14 +0700

    Update setproxywin.bat

--- setproxywin.bat
+++ setproxywin.bat
@@ -1,4 +1,6 @@
 @echo off
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 title Set up internet access...
 REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "socks=localhost:8080" /t REG_SZ /f
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /d "1" /t REG_SZ /f
