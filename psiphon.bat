9c6ff4aa40fe4c747535f5d82028c0c6c66d3cbc
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-26 23:14:08 +0700

    Update psiphon.bat

--- psiphon.bat
+++ psiphon.bat
@@ -1,3 +1,4 @@
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 curl -L -k -O https://github.com/kmille36/thuonghai/raw/master/setproxywin.bat
 curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/BraveBrowserSetup.exe
