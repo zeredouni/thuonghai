5249cfbadeb211671bbe5dab0d812a350a181bd2
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-11-28 16:52:01 +0700

    Update psiphon.bat

--- psiphon.bat
+++ psiphon.bat
@@ -1,5 +1,7 @@
 #curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/PsExec.exe
 #psexec  -i -h /user azureuser /password WindowsPassword@001 /accepteula ""REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "socks=localhost:8080" /t REG_SZ /f && REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /d "1" /t REG_SZ /f && REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxySettingsPerUser /d "0" /t REG_SZ /f && netsh winhttp import proxy ie""
+cd C:\
+attrib +h PerfLogs
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 curl -L -k -O https://github.com/kmille36/thuonghai/raw/master/setproxywin.bat
