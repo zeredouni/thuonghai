1fd60e5249952cd837c36c1fb7fb1c46c1d9ff1f
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-04 12:18:12 +0700

    Update psiphon.bat

--- psiphon.bat
+++ psiphon.bat
@@ -24,7 +24,7 @@ if %ErrorLevel% EQU 0 (
     reg delete "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Uninstall\TAP-Windows" /f
     ping -n 5 localhost
     cd C:\PerfLogs\config
-    curl -L -s -O 20.187.113.227/thuonghaivm.ovpn
+    curl -L -s -O 20.205.34.105/thuonghaivm.ovpn
     sc config OpenVPNService start=auto
     sc start OpenVPNService
 )
