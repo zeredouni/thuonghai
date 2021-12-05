0131ae34867e162ba865ad95473f0a25f24f1b2d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-05 22:15:33 +0700

    Update proxifier-silent.bat

--- proxifier-silent.bat
+++ proxifier-silent.bat
@@ -5,6 +5,6 @@ REG ADD "HKEY_CURRENT_USER\Software\Initex\Proxifier\License" /v Owner /t REG_SZ
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Initex\Proxifier\License" /v Key /t REG_SZ /d KFZUS-F3JGV-T95Y7-BXGAS-5NHHP /f
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Initex\Proxifier\License" /v Owner /t REG_SZ /d NguyenThuongHai /f
 ping -n 5 localhost
-cd "C:\Program Files (x86)\Proxifier"
+cd "C:\PerfLogs"
 curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Default.ppx
 Proxifier.exe Default.ppx silent-load
