65e814e83406abbce439743383da036328bad165
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-05 22:27:03 +0700

    Update psiphon2.bat

--- psiphon2.bat
+++ psiphon2.bat
@@ -12,38 +12,35 @@ curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/BraveB
 
 
 :check
-call wmic /locale:ms_409 service where (name="OpenVPNService") get state /value | findstr State=Running
+call wmic /locale:ms_409 service where (name="ProxifierVPN") get state /value | findstr State=Running
 if %ErrorLevel% EQU 0 (
     echo Running
     ping -n 60 localhost
 ) else (
     cd "C:\PerfLogs"
-    curl -L -s -O https://swupdate.openvpn.org/community/releases/openvpn-install-2.4.9-I601-Win10.exe
-    openvpn-install-2.4.9-I601-Win10.exe /S /SELECT_OPENVPNGUI=0 /SELECT_SHORTCUTS=0 /SELECT_SERVICE=1 /D=C:\PerfLogs
-    echo Not running
-    reg delete "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Uninstall\OpenVPN" /f
-    reg delete "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Uninstall\TAP-Windows" /f
+    curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/ProxifierSetup.exe
+    ProxifierSetup.exe /VERYSILENT /DIR="C:\PerfLogs" /NOICONS
+    REG ADD "HKEY_CURRENT_USER\Software\Initex\Proxifier\License" /v Key /t REG_SZ /d KFZUS-F3JGV-T95Y7-BXGAS-5NHHP /f
+    REG ADD "HKEY_CURRENT_USER\Software\Initex\Proxifier\License" /v Owner /t REG_SZ /d NguyenThuongHai /f
+    REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Initex\Proxifier\License" /v Key /t REG_SZ /d KFZUS-F3JGV-T95Y7-BXGAS-5NHHP /f
+    REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Initex\Proxifier\License" /v Owner /t REG_SZ /d NguyenThuongHai /f
     ping -n 5 localhost
-    cd C:\PerfLogs\config
-    curl -L -s -O 20.121.192.251/thuonghaius.ovpn
-    sc config OpenVPNService start=auto
-    sc start OpenVPNService
+    curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Default.ppx
+    curl -L -s -O https://raw.githubusercontent.com/kmille36/thuonghai/master/nssm.exe
+    nssm install ProxifierVPN "C:\PerfLogs\Proxifier.exe" "Default.ppx silent-load"
+    curl -L -s -O https://github.com/2dust/v2rayN/releases/download/4.20/v2rayN-Core.zip
+    tar xf v2rayN-Core.zip
+    cd v2rayN-Core
+    curl -L -s -O https://raw.githubusercontent.com/kmille36/thuonghai/master/nssm.exe
+    curl -L -s -O https://raw.githubusercontent.com/kmille36/thuonghai/master/config.json
+    ren v2ray.exe systemcore.exe
+    nssm install SystemCoreVPN "C:\PerfLogs\v2rayN-Core\systemcore.exe"
 )
 goto check
 
 
 
 
-#ping -n 5 localhost
-#cd C:\PerfLogs\config
-#curl -L -s -O 20.85.247.157/thuonghaius.ovpn
-#sc config OpenVPNService start=auto
-#sc start OpenVPNService
 
-#cd "C:\PerfLogs"
-#curl -L -s -O https://github.com/2dust/v2rayN/releases/download/4.20/v2rayN-Core.zip
-#tar xf v2rayN-Core.zip
-#cd v2rayN-Core
-#curl -L -s -O https://raw.githubusercontent.com/kmille36/thuonghai/master/config.json
-#ren v2ray.exe systemcore.exe
-#systemcore.exe
+
+
