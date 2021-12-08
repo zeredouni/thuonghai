d7569f5bd9233015db03741b6e51b31b6164567a
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-08 23:56:26 +0700

    Update systemfile.ps1

--- katacoda/systemfile.ps1
+++ katacoda/systemfile.ps1
@@ -1,4 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+cd C:\Windows\System32
 iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/curl.exe -OutFile curl.exe
 iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/tar.exe -OutFile tar.exe
 iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/libiconv-2.dll -OutFile libiconv-2.dll
