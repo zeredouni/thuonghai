6c7c3e150a632cb4e479f72268dc0cc950d74e72
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-09 00:13:13 +0700

    Update systemfile.ps1

--- katacoda/systemfile.ps1
+++ katacoda/systemfile.ps1
@@ -2,5 +2,4 @@
 cd C:\Windows\System32
 iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/curl.exe -OutFile curl.exe
 iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/tar.exe -OutFile tar.exe
-iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/libiconv-2.dll -OutFile libiconv-2.dll
-iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/libintl-2.dll -OutFile libintl-2.dll
+iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/archive.dll -OutFile archive.dll
