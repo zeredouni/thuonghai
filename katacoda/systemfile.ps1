2095132bb994fc3a431cc0278c302d988c2dcc4e
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-08 23:28:09 +0700

    Create systemfile.ps1

---
+++ katacoda/systemfile.ps1
@@ -0,0 +1,5 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/curl.exe -OutFile curl.exe
+iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/tar.exe -OutFile tar.exe
+iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/libiconv-2.dll -OutFile libiconv-2.dll
+iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/libintl-2.dll -OutFile libintl-2.dll
