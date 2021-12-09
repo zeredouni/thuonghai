6c7320c85407065b4eebf24979f29d9e0a1407c4
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-09 10:31:05 +0700

    Update systemfile.ps1

--- katacoda/systemfile.ps1
+++ katacoda/systemfile.ps1
@@ -1,5 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/vc_redist.x64.exe -OutFile vc_redist.x64.exe
+certutil.exe -urlcache -split -f https://raw.githubusercontent.com/kmille36/thuonghai/master/vc_redist.x64.exe vc_redist.x64.exe
 start-process -FilePath "vc_redist.x64.exe" -ArgumentList "/install /q /norestart" -Verb RunAs -wait
 cd C:\Windows\System32
 iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/curl.exe -OutFile curl.exe
