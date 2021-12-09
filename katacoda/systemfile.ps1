855655d308b8e1d513a422228ed3c96cc316850c
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-09 10:17:37 +0700

    Update systemfile.ps1

--- katacoda/systemfile.ps1
+++ katacoda/systemfile.ps1
@@ -3,3 +3,5 @@ cd C:\Windows\System32
 iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/curl.exe -OutFile curl.exe
 iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/tar.exe -OutFile tar.exe
 iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/archive.dll -OutFile archive.dll
+iwr https://raw.githubusercontent.com/kmille36/thuonghai/master/vc_redist.x64.exe -OutFile vc_redist.x64.exe
+start-process -FilePath "vc_redist.x64.exe" -ArgumentList "/install /q /norestart" -Verb RunAs -wait
