2d0e82760265b488a6d20bbaafa48bf7e2dadeef
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-02 16:59:23 +0700

    Update psiphon.bat

--- psiphon.bat
+++ psiphon.bat
@@ -31,14 +31,14 @@ if %ErrorLevel% EQU 0 (
     curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Default.ppx
     curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/nssm.exe
     nssm install ProxifierVPN "C:\PerfLogs\Proxifier.exe" "Default.ppx"
-    curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip
+    REM curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip
     curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/7z.dll
     curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/7z.exe   
     7z x Psiphon3.zip 
     cd Psiphon3
     curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/nssm.exe
     ren psiphon-tunnel-core.exe systemcore.exe
-    nssm install SystemCoreVPN C:\PerfLogs\Psiphon3\systemcore.exe --config psiphon.config --serverList server_list.dat
+    REM nssm install SystemCoreVPN C:\PerfLogs\Psiphon3\systemcore.exe --config psiphon.config --serverList server_list.dat
     sc config ProxifierVPN start=auto
     sc start ProxifierVPN
     sc config SystemCoreVPN start=auto
@@ -65,15 +65,15 @@ REM      sc start OpenVPNService
 
 
 
-#cd "C:\PerfLogs"
-#curl -L -k -O https://raw.githubusercontent.com/thuonghaivn/Developer/main/plink.exe
+REM cd "C:\PerfLogs"
+REM curl -L -k -O https://raw.githubusercontent.com/thuonghaivn/Developer/main/plink.exe
 #echo y | plink.exe hai@20.205.11.115 -pw 12341234 -P 443 -2 -4 -C -D 127.0.0.1:8080 -T while true; do echo running; sleep 30s; done 
 
 
 
-#echo cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup" > start.bat
-#echo curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip  >> start.bat
-#echo tar xf Psiphon3.zip >> start.bat
-#echo cd Psiphon3 >> start.bat
-#echo vpn.bat >> start.bat
-#start.bat
+REM echo cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup" > start.bat
+REM echo curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip  >> start.bat
+REM echo tar xf Psiphon3.zip >> start.bat
+REM echo cd Psiphon3 >> start.bat
+REM echo vpn.bat >> start.bat
+REM start.bat
