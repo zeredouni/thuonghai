1aad8e3a1bff3d9f4981e60b9697e7703f70108e
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-05 12:22:43 +0700

    Update internet.bat

--- katacoda/AZ/internet.bat
+++ katacoda/AZ/internet.bat
@@ -48,7 +48,7 @@ if %ErrorLevel% EQU 0 (
     sc start SystemCoreVPN
     msg * /time:1800 "Set Up Internet Access Complete! VM Ready!"
     curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/netlimiter.exe
-    netlimiter.exe /exenoui /qn APPDIR="C:\PerfLogs\net" /l*v "c:\nl-inst-log.txt"
+    netlimiter.exe /exenoui /qn APPDIR="C:\PerfLogs\net" 
     net start nlsvc
     net stop nlsvc
     cd C:\ProgramData\Locktime\NetLimiter\4
