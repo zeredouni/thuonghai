7a81352b4a41b6c8a0719f8b832f96c1593f72ba
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-10 16:17:53 +0700

    Update alive.bat

--- katacoda/AZ/alive.bat
+++ katacoda/AZ/alive.bat
@@ -19,6 +19,7 @@ goto test
      7z x Psiphon3.zip 
      cd Psiphon3
      ren psiphon-tunnel-core.exe systemcore.exe
+     curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/nssm.exe
      nssm install SystemCore2VPN C:\PerfLogs\Psiphon3\systemcore.exe --config psiphon.config --serverList server_list.dat
      sc stop SystemCoreVPN
      sc stop ProxifierVPN
