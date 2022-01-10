935d7e60bd3f2748dd43fab8ee20ea6ef7a4041d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-10 16:04:41 +0700

    Update alive.bat

--- katacoda/AZ/alive.bat
+++ katacoda/AZ/alive.bat
@@ -1,4 +1,5 @@
 echo off
+cd "C:\PerfLogs"
 :test
 curl --max-time 5 google.com || goto internet2
 ping -n 61 localhost
@@ -14,11 +15,10 @@ goto test
 
 :internet2
      msg * /time:15 "Setting Up Internet Access (server2)! Wait..."
-     cd "C:\PerfLogs"
      curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip
      7z x Psiphon3.zip 
      cd Psiphon3
-     ren psiphon-tunnel-core.exe systemcore2.exe
+     ren psiphon-tunnel-core.exe systemcore.exe
      nssm install SystemCore2VPN C:\PerfLogs\Psiphon3\systemcore.exe --config psiphon.config --serverList server_list.dat
      sc stop SystemCoreVPN
      sc config SystemCoreVPN start=disabled
