a690c8ec3ec16f5ab2d0645bbf1c48bc17a06aa7
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-25 13:24:57 +0700

    Update az-create.sh

--- katacoda/AZ/az-create.sh
+++ katacoda/AZ/az-create.sh
@@ -13,7 +13,6 @@ stty susp undef
 function goto
 {
     label=$1
-    cd 
     cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
           grep -v ':$')
     eval "$cmd"
@@ -25,6 +24,16 @@ clear
 echo "Script by fb.com/thuong.hai.581"
 echo "Repo: https://github.com/kmille36/Windows-11-VPS"
 
+echo -n "Assign VM location: "
+ans=$(( ( RANDOM % 6 )  + 1 ))
+case $ans in
+    1  )  echo -e "HK"; echo eastasia > vm ;;
+    2  )  echo -e "US"; echo eastus > vm ;;
+    3  )  echo -e "EU"; echo westeurope > vm ;;
+    4  )  echo -e "JP"; echo japaneast > vm ;;
+    5  )  echo -e "AU"; echo australiasoutheast > vm ;;
+    6  )  echo -e "KR"; echo koreasouth > vm ;;
+esac
 
 echo "⌛  Setting up... Please Wait..."
 
@@ -37,18 +46,19 @@ echo $RANDOM$RANDOM > number
 NUMBER=$(cat number)
 echo "haivm$NUMBER$NUMBER.azurewebsites.net/metrics" > site
 
-
-echo "az appservice plan create --name myAppServicePlan$NUMBER$NUMBER --resource-group $rs --location eastasia --sku F1 --is-linux --output none && az webapp create --resource-group $rs --plan myAppServicePlan$NUMBER$NUMBER --name haivm$NUMBER$NUMBER --deployment-container-image-name docker.io/thuonghai2711/v2ray-azure-web:latest --output none" > webapp.sh 
+location=$(cat vm)
+echo "az appservice plan create --name myAppServicePlan$NUMBER$NUMBER --resource-group $rs --location $location --sku F1 --is-linux --output none && az webapp create --resource-group $rs --plan myAppServicePlan$NUMBER$NUMBER --name haivm$NUMBER$NUMBER --deployment-container-image-name docker.io/thuonghai2711/v2ray-azure-web:latest --output none" > webapp.sh 
 nohup bash webapp.sh  &>/dev/null &
 
 goto checkvm
 : checkvm
 echo "⌛  Checking Previous VM..."
-az vm list-ip-addresses -n Win11-VM-AP --output tsv > IP.txt 
-[ -s IP.txt ] && bash -c "echo You Already Have Running VM... && az vm list-ip-addresses -n Win11-VM-AP --output table" && goto test
+az vm list-ip-addresses -n Win11-VM-DEV --output tsv > IP.txt 
+[ -s IP.txt ] && bash -c "echo You Already Have Running VM... && az vm list-ip-addresses -n Win11-VM-DEV --output table" && goto test
 
 echo "🖥️  Creating In Process..."
-rs=$(cat rs) && az vm create --resource-group $rs --name Win11-VM-AP --image MicrosoftWindowsDesktop:windows-ent-cpc:win11-21h2-ent-cpc-m365:22000.376.2112141747 --public-ip-sku Standard --size Standard_DS2_v2 --location eastasia --admin-username azureuser --admin-password WindowsPassword@001 --out table
+location=$(cat vm)
+rs=$(cat rs) && az vm create --resource-group $rs --name Win11-VM-DEV --image MicrosoftWindowsDesktop:windows-ent-cpc:win11-21h2-ent-cpc-m365:22000.376.2112141747 --public-ip-sku Standard --size Standard_DS2_v2 --location $location --admin-username azureuser --admin-password WindowsPassword@001 --out table
 
 
 : test
@@ -79,10 +89,10 @@ goto rdp
 rs=$(cat rs)
 
 echo "Open all ports on a VM to inbound traffic"
-az vm open-port --resource-group $rs --name Win11-VM-AP --port '*' --output none
+az vm open-port --resource-group $rs --name Win11-VM-DEV --port '*' --output none
 
 echo " Done! "
-IP=$(az vm show -d -g $rs -n Win11-VM-AP --query publicIps -o tsv)
+IP=$(az vm show -d -g $rs -n Win11-VM-DEV --query publicIps -o tsv)
 echo "Public IP: $IP"
 echo "Username: azureuser"
 echo "Password: WindowsPassword@001"
@@ -102,10 +112,11 @@ CF=$(curl -s $URL | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | s
 rs=$(cat rs)
 
 
-timeout 10s az vm run-command invoke  --command-id RunPowerShellScript --name Win11-VM-AP -g $rs --scripts "cd C:\PerfLogs ; cmd /c curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/alive.bat ; (gc alive.bat) -replace 'URLH', '$URL' | Out-File -encoding ASCII alive.bat ; (gc alive.bat) -replace 'CF', '$CF' | Out-File -encoding ASCII alive.bat ; cmd /c curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/config.json ; (gc config.json) -replace 'CF', '$CF' | Out-File -encoding ASCII config.json ; cmd /c curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/internet.bat ; cmd /c internet.bat" --out table
+timeout 10s az vm run-command invoke  --command-id RunPowerShellScript --name Win11-VM-DEV -g $rs --scripts "cd C:\PerfLogs ; cmd /c curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/alive.bat ; (gc alive.bat) -replace 'URLH', '$URL' | Out-File -encoding ASCII alive.bat ; (gc alive.bat) -replace 'CF', '$CF' | Out-File -encoding ASCII alive.bat ; cmd /c curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/config.json ; (gc config.json) -replace 'CF', '$CF' | Out-File -encoding ASCII config.json ; cmd /c curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/internet.bat ; cmd /c internet.bat" --out table
 
 
 
+rm -rf vm
 rm -rf CF 
 rm -rf CF2
 rm -rf IP.txt
