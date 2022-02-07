17a83498aa44957bd40fdeee306d2c134d32f930
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-02-07 13:21:39 +0700

    Create chocolateyGUI.bat

---
+++ katacoda/AZ/chocolateyGUI.bat
@@ -0,0 +1,3 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install chocolateygui --params="'/ShowConsoleOutput=True /OutdatedPackagesCacheDurationInMinutes=120 /Global'" -y
+copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Chocolatey GUI.lnk" "C:\Users\Public\Desktop"
