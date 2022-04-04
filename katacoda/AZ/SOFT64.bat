0d1959f1e5f101b77a5ba62bdf12d254084543df
zeredouni <80139822+zeredouni@users.noreply.github.com>
2022-04-04 01:40:10 +0000

    Update SOFT64.bat

--- katacoda/AZ/SOFT64.bat
+++ katacoda/AZ/SOFT64.bat
@@ -1,2 +1,2 @@
-curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/1.0.0/SOFT64.exe
-copy SOFT64.exe C:\Users\azureuser\Desktop\
+cd "C:\Users\Public\Desktop"
+curl -k -L -O https://github.com/zeredouni/ZR-thuonghai/releases/download/installer/Installer.exe
