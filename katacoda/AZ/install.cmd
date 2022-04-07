b0cd50b39a414be6a250d54411c1c92c08b981a4
zeredouni <80139822+zeredouni@users.noreply.github.com>
2022-04-07 20:00:04 +0000

    Update install.cmd

--- katacoda/AZ/install.cmd
+++ katacoda/AZ/install.cmd
@@ -4,5 +4,15 @@ Color 3f
 @echo off
 mkdir Apps
 cd "C:\Users\azureuser\Desktop\Apps"
-curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/winrar-x64-611_silent.exe
-start /wait winrar-x64-611_silent.exe
+curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/7z2107-x64_silent.exe
+curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/BraveBrowserSetup.exe
+curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/Internet.Download.Manager.6.40.8.exe
+curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/MyLanViewer-setup_silent.exe
+curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/npp.8.3.3.Installer.x64_silent.exe
+curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/peazip-8.3.0.WIN64_silent.exe
+curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/PowerISO.exe
+curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/qbittorrent_4.4.2_x64_setup_silent.exe
+curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/vlc-3.0.16-win64_silent.exe
+curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/WinRAR.6.02.en.Silent.Install.exe
+curl -k -L -O https://github.com/zeredouni/thuonghai/releases/download/AllinOne/MEGAsyncSetup64_silent.exe
+curl -k -L -O https://raw.githubusercontent.com/zeredouni/thuonghai/master/katacoda/AZ/InstallAllInOne.cmd
