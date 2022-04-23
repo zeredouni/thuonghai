763e2878fa68d24d75e4d85a70f1a67cc909a6d3
zeredouni <80139822+zeredouni@users.noreply.github.com>
2022-04-23 00:59:14 +0000

    Update alive.bat

--- katacoda/AZ/alive.bat
+++ katacoda/AZ/alive.bat
@@ -22,6 +22,5 @@ goto test
      sc config SystemCoreVPN start=disabled
      sc config SystemCore2VPN start=auto
      sc start SystemCore2VPN   
-     msg * /time:1800 "Set Up Internet Access (server2) Complete! VM Ready!"
      ping -n 10 localhost
      goto test
